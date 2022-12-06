e9b6ef129d7b126b9748179b171fcaf9ff58c580
Sunil kumar <sunilkumar3020@gmail.com>
2022-12-06 15:50:09 +0530

    project completed

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const passwordField = document.querySelector('input[type="password"]');
-console.log(passwordField);
-alert("hellow");
+let passwordField = document.querySelector("input");
+
+let eyeIcon = document.querySelector(".showBtn");
+
+function showpassword(e) {
+  e.target.classList.toggle("fa-eye-slash");
+  if (passwordField.getAttribute("type") === "password") {
+    passwordField.setAttribute("type", "text");
+  } else {
+    passwordField.setAttribute("type", "password");
+  }
+}
+
+eyeIcon.addEventListener("click", showpassword, true);
